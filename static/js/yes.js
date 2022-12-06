7a34b2ae3f966e9b083fb6683d23c2d3d0141c61
Hirak Desai <hirak214@gmail.com>
2022-12-06 12:50:03 +0530

    Final Working
    @itsreallyyash needs to update submit btn css

---
+++ static/js/yes.js
@@ -0,0 +1,30 @@
+function check() {
+    var q1=document.quiz.b1.value;
+    var q2=document.quiz1.b2.value;
+    var q3=document.quiz2.b3.value;
+    var q4=document.quiz3.b4.value;
+    var q5=document.quiz4.b5.value;
+    var q6=document.quiz5.b6.value;
+    var q7=document.quiz6.b7.value;
+    var q8=document.quiz7.b8.value;
+    var q9=document.quiz8.b9.value;
+    var q10=document.quiz9.b10.value;
+    var q11=document.quiz10.b11.value;
+    var q12=document.quiz11.b12.value;
+    var q13=document.quiz12.b13.value;
+    var q14=document.quiz13.b14.value;
+    var q15=document.quiz14.b15.value;
+    var q16=document.quiz15.b16.value;
+    var q17=document.quiz16.b17.value;
+    var q18=document.quiz17.b18.value;
+    var q19=document.quiz18.b19.value;
+    var q20=document.quiz19.b20.value;
+    var q21=document.quiz20.b21.value;
+    var q22=document.quiz21.b22.value;
+
+
+    var rank = [q1, q2, q3,q4,q5,q6,q7,q8,q9,q10,q11,q12,q13,q14,q15,q16,q17,q18,q19,q20,q21];
+    for(let i = 0; i < rank.length; i++){
+        document.write(rank[i]);
+      }
+; ;}
